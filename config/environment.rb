134f0074f8ac3e8e32728d83dc20c0dce9eeb81d
Josh Owens <joshua.owens@gmail.com>
2008-12-10 02:16:43 -0500

    Will Paginate gem.

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem 'rspec', :lib => "spec", :version => '1.1.11'
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
   config.gem "authlogic"
+  config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
