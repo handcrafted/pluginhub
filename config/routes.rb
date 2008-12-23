64bd9424bca4347fdfd27ea5424a5d1875cfc7b2
Josh Owens <joshua.owens@gmail.com>
2008-12-23 01:41:07 -0500

    Adding admin/systems route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resource :user_session
   
+  map.namespace :admin do |admin|
+    admin.resources :systems
+  end
   
   map.resources :pages
   
